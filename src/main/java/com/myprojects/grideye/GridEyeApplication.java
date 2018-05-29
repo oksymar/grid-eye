37fb962859684d6ea20e31083f7e9ae5bc2ae6a3
oksymar <mkmmiichchaall@gmail.com>
2018-05-29 02:54:12 +0200

    Initail commit

---
+++ src/main/java/com/myprojects/grideye/GridEyeApplication.java
@@ -0,0 +1,12 @@
+package com.myprojects.grideye;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GridEyeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GridEyeApplication.class, args);
+    }
+}
---
+++ src/test/java/com/myprojects/grideye/GridEyeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.myprojects.grideye;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class GridEyeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
